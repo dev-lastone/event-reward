43c8f6bd4c490c1b80bbb6c3927623961fa39227
jjong <akrTmwk@gmail.com>
2025-05-17 17:02:19 +0900

    refactor: event app

--- apps/event/src/event/event.controller.ts
+++ apps/event/src/event/event.controller.ts
@@ -1,12 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { EventService } from './event.service';
 
 @Controller()
 export class EventController {
   constructor(private readonly eventService: EventService) {}
-
-  @Get()
-  getHello(): string {
-    return this.eventService.getHello();
-  }
 }
