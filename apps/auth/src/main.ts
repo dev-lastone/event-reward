4119aaee657ce547d8747c1d14e837b321a7aeb4
jjong <akrTmwk@gmail.com>
2025-05-17 17:09:08 +0900

    refactor: swagger lib common 별도 함수 추출

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,33 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { swaggerSetting } from 'common/common/util/swagger.setting';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const options = new DocumentBuilder()
-    .setTitle('auth api')
-    .setVersion('1.0.0')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        name: 'Authorization',
-        in: 'header',
-      },
-      'jwt',
-    )
-    .build();
-
-  const document = SwaggerModule.createDocument(app, options);
-
-  SwaggerModule.setup('docs', app, document, {
-    swaggerOptions: {
-      persistAuthorization: true,
-      defaultModelsExpandDepth: -1,
-      defaultModelExpandDepth: 10,
-    },
-  });
+  swaggerSetting(app, 'auth');
 
   await app.listen(process.env.port ?? 3000);
 }
