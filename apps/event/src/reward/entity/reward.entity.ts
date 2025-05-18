c3946f721289444397d9dccc9ebe5c746ee99e72
jjong <akrTmwk@gmail.com>
2025-05-18 12:36:20 +0900

    refactor: reward entity type enum 적용

--- apps/event/src/reward/entity/reward.entity.ts
+++ apps/event/src/reward/entity/reward.entity.ts
@@ -1,13 +1,19 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
 
+export enum RewardType {
+  POINT = 'POINT',
+  ITEM = 'ITEM',
+  COUPON = 'COUPON',
+}
+
 @Schema()
 export class Reward {
   @Prop({ required: true })
   eventId: mongoose.Schema.Types.ObjectId;
 
-  @Prop({ required: true })
-  type: 'POINT' | 'ITEM' | 'COUPON';
+  @Prop({ enum: RewardType, required: true })
+  type: RewardType;
 
   @Prop({ required: true })
   value: string; // 포인트 숫자 or 아이템명 or 쿠폰코드 등
