d31f64492f528e2b181e03ea71f79a677abb49d7
jjong <akrTmwk@gmail.com>
2025-05-19 19:25:02 +0900

    feat: gateway auth register admin

---
+++ apps/gateway/src/auth/dto/register.dto.ts
@@ -0,0 +1,14 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class RegisterDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  loginId: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
