12bc834d887c23c8f953a5162c9936f8daa42c6c
jjong <akrTmwk@gmail.com>
2025-05-20 08:11:52 +0900

    fix: JwtPayload 포맷 변경

--- libs/common/src/type/jwt-payload.ts
+++ libs/common/src/type/jwt-payload.ts
@@ -1,8 +1,6 @@
 import { UserRole } from '../../../../apps/auth/src/user/entity/user.entity';
 
 export type JwtPayload = {
-  _id: string;
+  id: string;
   role: UserRole;
-  iat?: number;
-  exp?: number;
 };
