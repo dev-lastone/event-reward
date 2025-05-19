d92aee1295b28aec024125842e5370b56a01ede7
jjong <akrTmwk@gmail.com>
2025-05-19 18:31:12 +0900

    feat: gateway auth, event 연결

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { swaggerSetting } from 'common/util';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: parseInt(process.env.TCP_PORT) || 3001,
+    },
+  });
+
+  await app.startAllMicroservices();
 
   swaggerSetting(app, 'auth');
 
--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,9 +1,55 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        HTTP_PORT: Joi.number().required(),
+        AUTH_HOST: Joi.string().required(),
+        AUTH_TCP_PORT: Joi.number().required(),
+        EVENT_HOST: Joi.string().required(),
+        EVENT_TCP_PORT: Joi.number().required(),
+      }),
+    }),
+    ClientsModule.registerAsync({
+      clients: [
+        {
+          name: 'AUTH_SERVICE',
+          useFactory: (configService: ConfigService) => ({
+            transport: Transport.TCP,
+            options: {
+              host: configService.getOrThrow('AUTH_HOST'),
+              port: configService.getOrThrow('AUTH_TCP_PORT'),
+            },
+          }),
+          inject: [ConfigService],
+        },
+      ],
+      isGlobal: true,
+    }),
+    ClientsModule.registerAsync({
+      clients: [
+        {
+          name: 'EVENT_SERVICE',
+          useFactory: (configService: ConfigService) => ({
+            transport: Transport.TCP,
+            options: {
+              host: configService.getOrThrow('EVENT_HOST'),
+              port: configService.getOrThrow('EVENT_TCP_PORT'),
+            },
+          }),
+          inject: [ConfigService],
+        },
+      ],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
