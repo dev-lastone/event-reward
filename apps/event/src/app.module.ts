4549a0012ebf13ffd18e35c1d2413b55353154ce
jjong <akrTmwk@gmail.com>
2025-05-19 21:23:36 +0900

    feat: event auth tcp 연결

--- apps/event/src/app.module.ts
+++ apps/event/src/app.module.ts
@@ -6,6 +6,7 @@ import * as Joi from 'joi';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EventModule } from './event/event.module';
 import { EventRewardRequestModule } from './event-reward-request/event-reward-request.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -13,6 +14,8 @@ import { EventRewardRequestModule } from './event-reward-request/event-reward-re
       isGlobal: true,
       validationSchema: Joi.object({
         MONGO_DB_EVENT_URL: Joi.string().required(),
+        AUTH_HOST: Joi.string().required(),
+        AUTH_TCP_PORT: Joi.number().required(),
       }),
     }),
     MongooseModule.forRootAsync({
@@ -20,6 +23,22 @@ import { EventRewardRequestModule } from './event-reward-request/event-reward-re
         uri: configService.getOrThrow('MONGO_DB_EVENT_URL'),
       }),
       inject: [ConfigService],
+    }),
+    ClientsModule.registerAsync({
+      clients: [
+        {
+          name: 'AUTH_SERVICE',
+          useFactory: (configService: ConfigService) => ({
+            transport: Transport.TCP,
+            options: {
+              host: configService.getOrThrow('AUTH_HOST'),
+              port: configService.getOrThrow('AUTH_TCP_PORT'),
+            },
+          }),
+          inject: [ConfigService],
+        },
+      ],
+      isGlobal: true,
     }),
 
     EventModule,
