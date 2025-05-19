4271d46b5e169dfbb39b0d83c9bbc9072216675a
jjong <akrTmwk@gmail.com>
2025-05-19 18:31:12 +0900

    feat: gateway auth, event 연결

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { swaggerSetting } from 'common/util';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  swaggerSetting(app, 'gateway');
+
   await app.listen(3000);
 }
 bootstrap();
