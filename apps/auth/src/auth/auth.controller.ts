3f9252819e2b6323687a992eb0858ee279d3a57a
jjong <akrTmwk@gmail.com>
2025-05-20 03:43:24 +0900

    refactor: auth api endpoint 제거

--- apps/auth/src/auth/auth.controller.ts
+++ apps/auth/src/auth/auth.controller.ts
@@ -1,23 +1,12 @@
-import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserRegisterDto } from '../user/dto/user-register.dto';
-import { ApiOperation } from '@nestjs/swagger';
 import { UpdateRoleDto } from './dto/update-role.dto';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  @ApiOperation({
-    summary: '최초 관리자 등록 용도(실 사용시 내부망 전용)',
-  })
-  @Post('register-admin')
-  async registerAdmin(@Body() userRegisterDto: UserRegisterDto) {
-    await this.authService.registerAdmin(userRegisterDto);
-
-    return true;
-  }
 
   @MessagePattern({
     cmd: 'register-admin',
@@ -26,14 +15,6 @@ export class AuthController {
     await this.authService.registerAdmin(userRegisterDto);
 
     return true;
-  }
-
-  @ApiOperation({
-    summary: '유저 등록',
-  })
-  @Post('register')
-  async registerUser(@Body() userRegisterDto: UserRegisterDto) {
-    await this.authService.register(userRegisterDto);
   }
 
   @MessagePattern({
@@ -53,17 +34,6 @@ export class AuthController {
       userRegisterDto.username,
       userRegisterDto.password,
     );
-  }
-
-  @Patch('role/:username')
-  async updateUserRole(
-    @Param('username') username: string,
-    @Body() updateRoleDto: UpdateRoleDto,
-  ) {
-    await this.authService.updateRole({
-      username,
-      role: updateRoleDto.role,
-    });
   }
 
   @MessagePattern({
