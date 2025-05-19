da8df72900f90958ddab7ddf42492a3bc6f476fb
jjong <akrTmwk@gmail.com>
2025-05-19 20:38:53 +0900

    refactor: guard auth -> gateway

--- apps/gateway/src/auth/auth.controller.ts
+++ apps/gateway/src/auth/auth.controller.ts
@@ -1,10 +1,14 @@
 import { Body, Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { RegisterDto } from './dto/register.dto';
-import { LocalAuthGuard } from '../../../auth/src/guard/local-auth.guard';
+import { LocalAuthGuard } from '../guard/local-auth.guard';
 import { LoginDto } from '../../../auth/src/auth/dto/login.dto';
 import { UpdateAuthRoleDto } from './dto/update-auth-role.dto';
+import { JwtAuthGuard } from '../guard/jwt-auth.guard';
+import { RolesGuard } from '../guard/roles.guard';
+import { Roles } from '../decorator/roles.decorator';
+import { UserRole } from '../../../auth/src/user/entity/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -18,20 +22,29 @@ export class AuthController {
     await this.authService.registerAdmin(registerDto);
   }
 
-  @ApiOperation({
-    summary: '유저 등록',
-  })
-  @Post('register')
-  async registerUser(@Body() registerDto: RegisterDto) {
-    await this.authService.registerUser(registerDto);
-  }
-
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Req() req, @Body() loginDto: LoginDto) {
     return req.user;
   }
 
+  @ApiOperation({
+    summary: '유저 등록',
+  })
+  @ApiBearerAuth('jwt')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @Post('register')
+  async registerUser(@Body() registerDto: RegisterDto) {
+    await this.authService.registerUser(registerDto);
+  }
+
+  @ApiOperation({
+    summary: '유저 권한 변경',
+  })
+  @ApiBearerAuth('jwt')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
   @Post('role/:username')
   async updateRole(
     @Param('username') username: string,
