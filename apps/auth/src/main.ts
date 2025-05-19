567720920baf5380dbba418989f8511096bd87e0
jjong <akrTmwk@gmail.com>
2025-05-20 04:43:40 +0900

    chore: main validationPipeSetting

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { swaggerSetting } from 'common/util';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { validationPipeSetting } from 'common/util/validation-pipe.setting';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  validationPipeSetting(app);
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { swaggerSetting } from 'common/util';
+import { validationPipeSetting } from 'common/util/validation-pipe.setting';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  validationPipeSetting(app);
 
   swaggerSetting(app, 'gateway');
 
